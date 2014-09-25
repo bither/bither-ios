2e39d03c492162fc38d2bbe24fa6d167b06f6a98
songchenwen <emptyzone.0@gmail.com>
2014-09-25 12:53:55 +0800

    edit uentropy collector

--- bither-ios/XRandom/UEntropyCollector.m
+++ bither-ios/XRandom/UEntropyCollector.m
@@ -2,28 +2,118 @@
 //  UEntropyCollector.m
 //  bither-ios
 //
-//  Created by noname on 14-9-24.
-//  Copyright (c) 2014年 noname. All rights reserved.
+//  Copyright 2014 http://Bither.net
 //
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 
 #import "UEntropyCollector.h"
 
-@interface UEntropyCollector()
-
+@interface UEntropyCollector(){
+    dispatch_queue_t queue;
+    BOOL paused;
+    BOOL shouldCollectData;
+}
 @end
 
 @implementation UEntropyCollector
 
+-(instancetype)initWithDelegate:(NSObject<UEntropyDelegate>*) delegate{
+    self = [super init];
+    if(self){
+        self.delegate = delegate;
+        self.sources = [NSMutableSet set];
+        paused = YES;
+        shouldCollectData = NO;
+    }
+    return self;
+}
+
+-(void)addSource:(NSObject<UEntropySource>*)source,...{
+    va_list list;
+    va_start(list, source);
+    while (YES)
+    {
+        NSObject<UEntropySource> *s= va_arg(list, NSObject<UEntropySource> *);
+        if (s) {
+            [self addSingleSource:s];
+        }else{
+            break;
+        }
+    }
+    va_end(list);
+}
+
+-(void)addSingleSource:(NSObject<UEntropySource>*)source{
+    [self.sources addObject:source];
+    if (!paused) {
+        [source onResume];
+    }
+}
+
 -(void)onNewData:(NSData*)data fromSource:(NSObject<UEntropySource>*) source{
 
 }
+
 -(void)onError:(NSError*)error fromSource:(NSObject<UEntropySource>*) source{
-    
+    NSLog(@"uentropy collector source %@ error %@", source.name, error.description);
+    [source onPause];
+    if([self.sources containsObject:source]){
+        [self.sources removeObject:source];
+    }
+    if(self.sources.count == 0){
+        if(self.delegate && [self.delegate respondsToSelector:@selector(onNoSourceAvailable)]){
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [self.delegate onNoSourceAvailable];
+            });
+        }
+    }
 }
+
+-(NSData*)nextBytes:(int)length{
+    return nil;
+}
+
+-(void)start{
+    if(shouldCollectData){
+        return;
+    }
+    shouldCollectData = YES;
+}
+
+-(void)stop{
+    if(!shouldCollectData){
+        return;
+    }
+    shouldCollectData = NO;
+}
+
 -(void)onResume{
+    if(!paused){
+        return;
+    }
+    paused = NO;
+    for(NSObject<UEntropySource>* s in self.sources){
+        [s onResume];
+    }
 }
 
 -(void)onPause{
-
+    if(paused){
+        return;
+    }
+    paused = YES;
+    for(NSObject<UEntropySource>* s in self.sources){
+        [s onPause];
+    }
 }
 @end
--- bither-ios/XRandom/UEntropyViewController.m
+++ bither-ios/XRandom/UEntropyViewController.m
@@ -2,15 +2,25 @@
 //  UEntropyViewController.m
 //  bither-ios
 //
-//  Created by noname on 14-9-24.
-//  Copyright (c) 2014年 noname. All rights reserved.
+//  Copyright 2014 http://Bither.net
 //
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 
 #import "UEntropyViewController.h"
 #import "UEntropyCamera.h"
 #import "UEntropyCollector.h"
 
-@interface UEntropyViewController ()
+@interface UEntropyViewController ()<UEntropyDelegate>
 @property UEntropyCollector* collector;
 @property UEntropyCamera* camera;
 @end
@@ -30,7 +40,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.collector = [[UEntropyCollector alloc]init];
+    self.collector = [[UEntropyCollector alloc]initWithDelegate:self];
     self.camera = [[UEntropyCamera alloc]initWithViewController:self andCollector:self.collector];
 }
 
@@ -46,6 +56,10 @@
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
+}
+
+-(void)onNoSourceAvailable{
+    
 }
 
 -(BOOL)prefersStatusBarHidden{
