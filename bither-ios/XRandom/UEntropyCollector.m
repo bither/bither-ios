f6d87fc6bd251032faf1ec980113de4549e2c4c3
songchenwen <emptyzone.0@gmail.com>
2014-09-25 14:40:28 +0800

    piped input and output stream

--- bither-ios/XRandom/UEntropyCollector.m
+++ bither-ios/XRandom/UEntropyCollector.m
@@ -22,8 +22,12 @@
     dispatch_queue_t queue;
     BOOL paused;
     BOOL shouldCollectData;
+    NSInputStream *input;
+    NSOutputStream *output;
 }
 @end
+
+#define kUEntropyPoolSize (32 * 200)
 
 @implementation UEntropyCollector
 
@@ -32,6 +36,7 @@
     if(self){
         self.delegate = delegate;
         self.sources = [NSMutableSet set];
+        queue = dispatch_queue_create("UEntropyCollector", NULL);
         paused = YES;
         shouldCollectData = NO;
     }
@@ -61,7 +66,14 @@
 }
 
 -(void)onNewData:(NSData*)data fromSource:(NSObject<UEntropySource>*) source{
-
+    if(shouldCollectData){
+        dispatch_async(queue, ^{
+            if(shouldCollectData && output && output.hasSpaceAvailable){
+                NSInteger actuallyWriten = [output write:data.bytes maxLength:data.length];
+                NSLog(@"write %lu/%lu data to uentropy pool from", actuallyWriten, data.length);
+            }
+        });
+    }
 }
 
 -(void)onError:(NSError*)error fromSource:(NSObject<UEntropySource>*) source{
@@ -80,7 +92,23 @@
 }
 
 -(NSData*)nextBytes:(int)length{
-    return nil;
+    if(!shouldCollectData || !input){
+        return nil;
+    }
+    NSMutableData* data = [NSMutableData dataWithLength:length];
+    NSUInteger dataNeeded = length;
+    while (dataNeeded > 0) {
+        if(input.hasBytesAvailable){
+            NSInteger outcome = [input read:data.mutableBytes + (length - dataNeeded) maxLength:dataNeeded];
+            if(outcome < 0){
+                NSLog(@"uentropy collector read error");
+                [self.delegate onNoSourceAvailable];
+                return nil;
+            }
+            dataNeeded -= outcome;
+        }
+    }
+    return data;
 }
 
 -(void)start{
@@ -88,6 +116,15 @@
         return;
     }
     shouldCollectData = YES;
+    NSInputStream *inputStr;
+    NSOutputStream *outputStr;
+    [self createBoundInputStream:&inputStr outputStream:&outputStr bufferSize:kUEntropyPoolSize];
+    if(!inputStr || !outputStr){
+        [self.delegate onNoSourceAvailable];
+    }
+    input = inputStr;
+    output = outputStr;
+    [output open];
 }
 
 -(void)stop{
@@ -95,6 +132,10 @@
         return;
     }
     shouldCollectData = NO;
+    [output close];
+    [input close];
+    output = nil;
+    input = nil;
 }
 
 -(void)onResume{
@@ -114,6 +155,29 @@
     paused = YES;
     for(NSObject<UEntropySource>* s in self.sources){
         [s onPause];
+    }
+}
+
+- (void)createBoundInputStream:(NSInputStream **)inputStreamPtr outputStream:(NSOutputStream **)outputStreamPtr bufferSize:(NSUInteger)bufferSize
+{
+    CFReadStreamRef     readStream;
+    CFWriteStreamRef    writeStream;
+    
+    assert( (inputStreamPtr != NULL) || (outputStreamPtr != NULL) );
+    
+    readStream = NULL;
+    writeStream = NULL;
+    CFStreamCreateBoundPair(NULL,
+                            ((inputStreamPtr  != nil) ? &readStream : NULL),
+                            ((outputStreamPtr != nil) ? &writeStream : NULL),
+                            (CFIndex) bufferSize
+                            );
+    
+    if (inputStreamPtr != NULL) {
+        *inputStreamPtr  = CFBridgingRelease(readStream);
+    }
+    if (outputStreamPtr != NULL) {
+        *outputStreamPtr = CFBridgingRelease(writeStream);
     }
 }
 @end
