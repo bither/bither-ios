45e9050138eaf1f1f3bc5a925c62ba9a584bc1ac
songchenwen <emptyzone.0@gmail.com>
2014-09-26 18:14:21 +0800

    interact with xrandom

--- bither-ios/XRandom/UEntropyViewController.h
+++ bither-ios/XRandom/UEntropyViewController.h
@@ -16,8 +16,8 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
-#import "ZBarReaderController.h"
+#import <UIKit/UIKit.h>
 
 @interface UEntropyViewController : UIViewController
-
+-(instancetype)initWithCount:(NSUInteger)count password:(NSString*)password;
 @end
