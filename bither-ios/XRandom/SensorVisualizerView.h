68dec989c30d458a4b383bcd3537abe705e2dc06
songchenwen <emptyzone.0@gmail.com>
2014-09-28 16:21:38 +0800

    add icons

---
+++ bither-ios/XRandom/SensorVisualizerView.h
@@ -0,0 +1,13 @@
+//
+//  SensorVisualizerView.h
+//  bither-ios
+//
+//  Created by noname on 14-9-28.
+//  Copyright (c) 2014年 noname. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SensorVisualizerView : UIView
+
+@end
---
+++ bither-ios/XRandom/SensorVisualizerView.m
@@ -0,0 +1,21 @@
+//
+//  SensorVisualizerView.m
+//  bither-ios
+//
+//  Created by noname on 14-9-28.
+//  Copyright (c) 2014年 noname. All rights reserved.
+//
+
+#import "SensorVisualizerView.h"
+
+@implementation SensorVisualizerView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
