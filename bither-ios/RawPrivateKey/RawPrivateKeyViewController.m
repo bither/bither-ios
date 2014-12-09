eb257b3906454a1e6ebcadd699369877517760c4
songchenwen <emptyzone.0@gmail.com>
2014-12-09 17:02:20 +0800

    edit raw private key input

--- bither-ios/RawPrivateKey/RawPrivateKeyViewController.m
+++ bither-ios/RawPrivateKey/RawPrivateKeyViewController.m
@@ -36,21 +36,30 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.vData.restrictedSize = CGSizeMake(self.vData.frame.size.width, self.vInput.frame.size.height * 0.6f);
+    self.vData.restrictedSize = CGSizeMake(self.vData.frame.size.width, self.vInput.frame.size.height * 0.52f);
     self.vData.dataSize = CGSizeMake(16, 16);
+    self.vButtons.frame = CGRectMake(self.vButtons.frame.origin.x, CGRectGetMaxY(self.vData.frame), self.vButtons.frame.size.width, self.vInput.frame.size.height - CGRectGetMaxY(self.vData.frame));
+}
+
+-(void)addData:(BOOL)d{
+    if(self.vData.filledDataLength < self.vData.dataLength){
+        [self.vData addData:d];
+    }
 }
 
 - (IBAction)zeroPressed:(id)sender {
-    
+    [self addData:NO];
 }
 
 - (IBAction)onePressed:(id)sender {
-    
+    [self addData:YES];
 }
+
 - (IBAction)addPressed:(id)sender {
 }
 
 - (IBAction)backPressed:(id)sender {
+    [self.navigationController popViewControllerAnimated:YES];
 }
 
 
