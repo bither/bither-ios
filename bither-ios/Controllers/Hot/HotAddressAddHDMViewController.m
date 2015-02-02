65352ec0c8363abec7a6c09612a5a9232d82698f
songchenwen <emptyzone.0@gmail.com>
2015-02-02 12:43:33 +0800

    add hdm hot add storyboard

---
+++ bither-ios/Controllers/Hot/HotAddressAddHDMViewController.m
@@ -0,0 +1,37 @@
+//
+//  HotAddressAddHDMViewController.m
+//  bither-ios
+//
+//  Created by 宋辰文 on 15/2/2.
+//  Copyright (c) 2015年 宋辰文. All rights reserved.
+//
+
+#import "HotAddressAddHDMViewController.h"
+
+@interface HotAddressAddHDMViewController ()
+
+@end
+
+@implementation HotAddressAddHDMViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
