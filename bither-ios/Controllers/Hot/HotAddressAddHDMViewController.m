cb8ea39047535593c71444605b80098c535b6687
songchenwen <emptyzone.0@gmail.com>
2015-02-02 12:53:37 +0800

    add hot hdm add outlets

--- bither-ios/Controllers/Hot/HotAddressAddHDMViewController.m
+++ bither-ios/Controllers/Hot/HotAddressAddHDMViewController.m
@@ -9,6 +9,17 @@
 #import "HotAddressAddHDMViewController.h"
 
 @interface HotAddressAddHDMViewController ()
+@property (weak, nonatomic) IBOutlet UIView *vContainer;
+@property (weak, nonatomic) IBOutlet UIView *vBg;
+@property (weak, nonatomic) IBOutlet UIImageView *ivHotLight;
+@property (weak, nonatomic) IBOutlet UIImageView *ivColdLight;
+@property (weak, nonatomic) IBOutlet UIImageView *ivServerLight;
+@property (weak, nonatomic) IBOutlet UIButton *btnHot;
+@property (weak, nonatomic) IBOutlet UIButton *btnCold;
+@property (weak, nonatomic) IBOutlet UIButton *btnServer;
+@property (weak, nonatomic) IBOutlet UILabel *lblHot;
+@property (weak, nonatomic) IBOutlet UILabel *lblCold;
+@property (weak, nonatomic) IBOutlet UILabel *lblServer;
 
 @end
 
@@ -19,19 +30,16 @@
     // Do any additional setup after loading the view.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)hotPressed:(id)sender {
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (IBAction)coldPressed:(id)sender {
 }
-*/
+
+- (IBAction)serverPressed:(id)sender {
+}
+
+- (IBAction)hdmInfoPressed:(id)sender {
+}
 
 @end
