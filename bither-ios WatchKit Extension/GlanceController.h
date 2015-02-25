c606869b1a8d7c8854eb9d0246ff3ca716f2a0b8
songchenwen <emptyzone.0@gmail.com>
2015-02-25 12:05:41 +0800

    initialize watch app

---
+++ bither-ios WatchKit Extension/GlanceController.h
@@ -0,0 +1,14 @@
+//
+//  GlanceController.h
+//  bither-ios WatchKit Extension
+//
+//  Created by 宋辰文 on 15/2/25.
+//  Copyright (c) 2015年 Bither. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface GlanceController : WKInterfaceController
+
+@end
