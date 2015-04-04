aa055c2df35ac6c0b2b4273f0fcddf5211eaf9a5
songchenwen <sanvinsung@gmail.com>
2015-04-04 11:29:35 +0800

    change network timeout to 10 seconds

--- bither-ios/Api/BaseApi.h
+++ bither-ios/Api/BaseApi.h
@@ -18,6 +18,8 @@
 #import <Foundation/Foundation.h>
 #import "BitherEngine.h"
 
+#define kMKNetworkKitRequestTimeOutInSeconds 10
+
 #define BITHER_GET_COOKIE_URL @"api/v1/cookie"
 #define BITHER_GET_ONE_SPVBLOCK_API @"api/v1/block/spv/one"
 #define BITHER_IN_SIGNATURES_API  @"api/v1/address/%@/insignature/%d"
