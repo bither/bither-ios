ff5628a33a2532b56caa3173d0b521038cff521e
songchenwen <sanvinsung@gmail.com>
2015-04-29 13:06:01 +0800

    add hd account to total balance

--- bither-ios/UIBase/Dialog/DialogTotalBalance.m
+++ bither-ios/UIBase/Dialog/DialogTotalBalance.m
@@ -28,82 +28,147 @@
 
 #define kForegroundInsetsRate (0.05f)
 #define kChartSize (260)
-#define kChartSizeSmall (230)
+#define kChartSizeSmall (220)
 #define kTopLabelFontSize (18)
 #define kVerticalGap (5)
 #define kBottomLabelFontSize (13)
 #define kBottomHorizontalMargin (15)
 
-@interface DialogTotalBalance (){
+@interface DialogTotalBalance () {
     int64_t total;
+    int64_t hd;
     int64_t hdm;
     int64_t hot;
     int64_t cold;
     double price;
 }
-@property PieChartView* chart;
+@property PieChartView *chart;
 @end
 
 @implementation DialogTotalBalance
 
--(instancetype)init{
+- (instancetype)init {
     self = [super initWithFrame:CGRectMake(0, 0, kChartSize, kChartSize)];
-    if(self){
+    if (self) {
         [self firstConfigure];
     }
     return self;
 }
--(void)firstConfigure{
+
+- (void)firstConfigure {
+    hd = 0;
     hdm = 0;
     hot = 0;
     cold = 0;
     total = 0;
     price = [MarketUtil getDefaultNewPrice];
-    NSArray* allAddresses = [BTAddressManager instance].allAddresses;
-    for(BTAddress* a in allAddresses){
-        if(a.isHDM){
+    NSArray *allAddresses = [BTAddressManager instance].allAddresses;
+    for (BTAddress *a in allAddresses) {
+        if (a.isHDM) {
             hdm += a.balance;
-        }else if(a.hasPrivKey){
-            hot+= a.balance;
-        }else{
-            cold+= a.balance;
+        } else if (a.hasPrivKey) {
+            hot += a.balance;
+        } else {
+            cold += a.balance;
         }
         total += a.balance;
     }
-    
+
+    if ([BTAddressManager instance].hasHDAccount) {
+        hd = [BTAddressManager instance].hdAccount.balance;
+        total += hd;
+    }
+
     CGFloat chartSize = kChartSize;
-    if(hdm > 0 && hot > 0 && cold > 0 && [UIScreen mainScreen].bounds.size.height <= 480){
+    int rowCount = (hdm > 0 ? 1 : 0) + (hot > 0 ? 1 : 0) + (cold > 0 ? 1 : 0) + (hd > 0 ? 1 : 0);
+    if (rowCount > 2 && [UIScreen mainScreen].bounds.size.height <= 480) {
         chartSize = kChartSizeSmall;
     }
-    
+
     self.bgInsets = UIEdgeInsetsMake(14, 6, 14, 6);
-    UILabel* topLabel = [[UILabel alloc]initWithFrame:CGRectMake(0, 0, self.frame.size.width, kTopLabelFontSize * 1.2)];
+    UILabel *topLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, self.frame.size.width, kTopLabelFontSize * 1.2)];
     topLabel.font = [UIFont systemFontOfSize:kTopLabelFontSize];
     topLabel.textColor = [UIColor whiteColor];
     topLabel.textAlignment = NSTextAlignmentCenter;
     topLabel.text = [NSString stringWithFormat:@"%@%@", NSLocalizedString(@"Total BTC prefix", nil), [UnitUtil stringForAmount:total]];
     [self addSubview:topLabel];
 
-    CGFloat left = (self.frame.size.width - chartSize)/2;
-    self.chart = [[PieChartView alloc]initWithFrame:CGRectMake(chartSize * kForegroundInsetsRate + left, CGRectGetMaxY(topLabel.frame) + kVerticalGap + chartSize * kForegroundInsetsRate, chartSize - chartSize * kForegroundInsetsRate * 2, chartSize - chartSize * kForegroundInsetsRate * 2)];
+    CGFloat left = (self.frame.size.width - chartSize) / 2;
+    self.chart = [[PieChartView alloc] initWithFrame:CGRectMake(chartSize * kForegroundInsetsRate + left, CGRectGetMaxY(topLabel.frame) + kVerticalGap + chartSize * kForegroundInsetsRate, chartSize - chartSize * kForegroundInsetsRate * 2, chartSize - chartSize * kForegroundInsetsRate * 2)];
     [self addSubview:self.chart];
-    UIImageView* ivForeground = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"pie_mask"]];
+    UIImageView *ivForeground = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"pie_mask"]];
     ivForeground.frame = CGRectMake(left, CGRectGetMaxY(topLabel.frame) + kVerticalGap, chartSize, chartSize);
     [self addSubview:ivForeground];
 
     CGFloat bottom = CGRectGetMaxY(ivForeground.frame);
 
-    NSString * symbol= [BitherSetting getCurrencySymbol:[[UserDefaultsUtil instance] getDefaultCurrency]];
-
-    if(hdm > 0){
-        UILabel* lbl = [[UILabel alloc]initWithFrame:CGRectMake(kBottomHorizontalMargin, bottom + kVerticalGap, self.frame.size.width - kBottomHorizontalMargin * 2, kBottomLabelFontSize * 1.2)];
+    NSString *symbol = [BitherSetting getCurrencySymbol:[[UserDefaultsUtil instance] getDefaultCurrency]];
+    if (hd > 0) {
+        UILabel *lbl = [[UILabel alloc] initWithFrame:CGRectMake(kBottomHorizontalMargin, bottom + kVerticalGap, self.frame.size.width - kBottomHorizontalMargin * 2, kBottomLabelFontSize * 1.2)];
         lbl.font = [UIFont systemFontOfSize:kBottomLabelFontSize];
         lbl.textColor = [UIColor whiteColor];
         lbl.textAlignment = NSTextAlignmentLeft;
-        lbl.attributedText = [self stringAddDotColor:[self.chart colorForIndex:0] string:@"HDM"];
+        lbl.attributedText = [self stringAddDotColor:[self.chart colorForIndex:0] string:NSLocalizedString(@"address_group_hd", nil)];
         [self addSubview:lbl];
 
-        lbl = [[UILabel alloc]initWithFrame:CGRectMake(kBottomHorizontalMargin, bottom + kVerticalGap, self.frame.size.width - kBottomHorizontalMargin * 2, kBottomLabelFontSize * 1.2)];
+        lbl = [[UILabel alloc] initWithFrame:CGRectMake(kBottomHorizontalMargin, bottom + kVerticalGap, self.frame.size.width - kBottomHorizontalMargin * 2, kBottomLabelFontSize * 1.2)];
+        lbl.font = [UIFont systemFontOfSize:kBottomLabelFontSize];
+        lbl.textColor = [UIColor whiteColor];
+        lbl.textAlignment = NSTextAlignmentRight;
+        lbl.attributedText = [UnitUtil stringWithSymbolForAmount:hd withFontSize:kBottomLabelFontSize color:lbl.textColor];
+        [self addSubview:lbl];
+
+        bottom = CGRectGetMaxY(lbl.frame);
+
+        if (price > 0) {
+            lbl = [[UILabel alloc] initWithFrame:CGRectMake(kBottomHorizontalMargin, bottom + kVerticalGap - 4, self.frame.size.width - kBottomHorizontalMargin * 2, kBottomLabelFontSize * 1.2)];
+            lbl.font = [UIFont systemFontOfSize:kBottomLabelFontSize];
+            lbl.textColor = [UIColor whiteColor];
+            lbl.textAlignment = NSTextAlignmentRight;
+            lbl.text = [NSString stringWithFormat:@"%@ %.2f", symbol, (price * hd) / pow(10, 8)];
+            [self addSubview:lbl];
+
+            bottom = CGRectGetMaxY(lbl.frame);
+        }
+    }
+    if (hdm > 0) {
+        UILabel *lbl = [[UILabel alloc] initWithFrame:CGRectMake(kBottomHorizontalMargin, bottom + kVerticalGap, self.frame.size.width - kBottomHorizontalMargin * 2, kBottomLabelFontSize * 1.2)];
+        lbl.font = [UIFont systemFontOfSize:kBottomLabelFontSize];
+        lbl.textColor = [UIColor whiteColor];
+        lbl.textAlignment = NSTextAlignmentLeft;
+        lbl.attributedText = [self stringAddDotColor:[self.chart colorForIndex:1] string:@"HDM"];
+        [self addSubview:lbl];
+
+        lbl = [[UILabel alloc] initWithFrame:CGRectMake(kBottomHorizontalMargin, bottom + kVerticalGap, self.frame.size.width - kBottomHorizontalMargin * 2, kBottomLabelFontSize * 1.2)];
+        lbl.font = [UIFont systemFontOfSize:kBottomLabelFontSize];
+        lbl.textColor = [UIColor whiteColor];
+        lbl.textAlignment = NSTextAlignmentRight;
+        lbl.attributedText = [UnitUtil stringWithSymbolForAmount:hdm withFontSize:kBottomLabelFontSize color:lbl.textColor];
+        [self addSubview:lbl];
+
+        bottom = CGRectGetMaxY(lbl.frame);
+
+        if (price > 0) {
+            lbl = [[UILabel alloc] initWithFrame:CGRectMake(kBottomHorizontalMargin, bottom + kVerticalGap - 4, self.frame.size.width - kBottomHorizontalMargin * 2, kBottomLabelFontSize * 1.2)];
+            lbl.font = [UIFont systemFontOfSize:kBottomLabelFontSize];
+            lbl.textColor = [UIColor whiteColor];
+            lbl.textAlignment = NSTextAlignmentRight;
+            lbl.text = [NSString stringWithFormat:@"%@ %.2f", symbol, (price * hdm) / pow(10, 8)];
+            [self addSubview:lbl];
+
+            bottom = CGRectGetMaxY(lbl.frame);
+        }
+    }
+
+    if (hot > 0) {
+        UILabel *lbl = [[UILabel alloc] initWithFrame:CGRectMake(kBottomHorizontalMargin, bottom + kVerticalGap, self.frame.size.width - kBottomHorizontalMargin * 2, kBottomLabelFontSize * 1.2)];
+        lbl.font = [UIFont systemFontOfSize:kBottomLabelFontSize];
+        lbl.textColor = [UIColor whiteColor];
+        lbl.textAlignment = NSTextAlignmentLeft;
+        lbl.attributedText = [self stringAddDotColor:[self.chart colorForIndex:2] string:NSLocalizedString(@"Hot Wallet Address", nil)];
+        [self addSubview:lbl];
+
+        lbl = [[UILabel alloc] initWithFrame:CGRectMake(kBottomHorizontalMargin, bottom + kVerticalGap, self.frame.size.width - kBottomHorizontalMargin * 2, kBottomLabelFontSize * 1.2)];
         lbl.font = [UIFont systemFontOfSize:kBottomLabelFontSize];
         lbl.textColor = [UIColor whiteColor];
         lbl.textAlignment = NSTextAlignmentRight;
@@ -112,56 +177,27 @@
 
         bottom = CGRectGetMaxY(lbl.frame);
 
-        if(price > 0){
-            lbl = [[UILabel alloc]initWithFrame:CGRectMake(kBottomHorizontalMargin, bottom + kVerticalGap - 4, self.frame.size.width - kBottomHorizontalMargin * 2, kBottomLabelFontSize * 1.2)];
+        if (price > 0) {
+            lbl = [[UILabel alloc] initWithFrame:CGRectMake(kBottomHorizontalMargin, bottom + kVerticalGap - 4, self.frame.size.width - kBottomHorizontalMargin * 2, kBottomLabelFontSize * 1.2)];
             lbl.font = [UIFont systemFontOfSize:kBottomLabelFontSize];
             lbl.textColor = [UIColor whiteColor];
             lbl.textAlignment = NSTextAlignmentRight;
-            lbl.text = [NSString stringWithFormat:@"%@ %.2f", symbol ,(price * hdm)/pow(10, 8)];
+            lbl.text = [NSString stringWithFormat:@"%@ %.2f", symbol, (price * hot) / pow(10, 8)];
             [self addSubview:lbl];
 
             bottom = CGRectGetMaxY(lbl.frame);
         }
     }
 
-    if(hot > 0){
-        UILabel* lbl = [[UILabel alloc]initWithFrame:CGRectMake(kBottomHorizontalMargin, bottom + kVerticalGap, self.frame.size.width - kBottomHorizontalMargin * 2, kBottomLabelFontSize * 1.2)];
+    if (cold > 0) {
+        UILabel *lbl = [[UILabel alloc] initWithFrame:CGRectMake(kBottomHorizontalMargin, bottom + kVerticalGap, self.frame.size.width - kBottomHorizontalMargin * 2, kBottomLabelFontSize * 1.2)];
         lbl.font = [UIFont systemFontOfSize:kBottomLabelFontSize];
         lbl.textColor = [UIColor whiteColor];
         lbl.textAlignment = NSTextAlignmentLeft;
-        lbl.attributedText = [self stringAddDotColor:[self.chart colorForIndex:1] string:NSLocalizedString(@"Hot Wallet Address", nil)];
+        lbl.attributedText = [self stringAddDotColor:[self.chart colorForIndex:3] string:NSLocalizedString(@"Cold Wallet Address", nil)];
         [self addSubview:lbl];
 
-        lbl = [[UILabel alloc]initWithFrame:CGRectMake(kBottomHorizontalMargin, bottom + kVerticalGap, self.frame.size.width - kBottomHorizontalMargin * 2, kBottomLabelFontSize * 1.2)];
-        lbl.font = [UIFont systemFontOfSize:kBottomLabelFontSize];
-        lbl.textColor = [UIColor whiteColor];
-        lbl.textAlignment = NSTextAlignmentRight;
-        lbl.attributedText = [UnitUtil stringWithSymbolForAmount:hot withFontSize:kBottomLabelFontSize color:lbl.textColor];
-        [self addSubview:lbl];
-
-        bottom = CGRectGetMaxY(lbl.frame);
-
-        if(price > 0){
-            lbl = [[UILabel alloc]initWithFrame:CGRectMake(kBottomHorizontalMargin, bottom + kVerticalGap - 4, self.frame.size.width - kBottomHorizontalMargin * 2, kBottomLabelFontSize * 1.2)];
-            lbl.font = [UIFont systemFontOfSize:kBottomLabelFontSize];
-            lbl.textColor = [UIColor whiteColor];
-            lbl.textAlignment = NSTextAlignmentRight;
-            lbl.text = [NSString stringWithFormat:@"%@ %.2f", symbol ,(price * hot)/pow(10, 8)];
-            [self addSubview:lbl];
-
-            bottom = CGRectGetMaxY(lbl.frame);
-        }
-    }
-
-    if(cold > 0){
-        UILabel* lbl = [[UILabel alloc]initWithFrame:CGRectMake(kBottomHorizontalMargin, bottom + kVerticalGap, self.frame.size.width - kBottomHorizontalMargin * 2, kBottomLabelFontSize * 1.2)];
-        lbl.font = [UIFont systemFontOfSize:kBottomLabelFontSize];
-        lbl.textColor = [UIColor whiteColor];
-        lbl.textAlignment = NSTextAlignmentLeft;
-        lbl.attributedText = [self stringAddDotColor:[self.chart colorForIndex:2] string:NSLocalizedString(@"Cold Wallet Address", nil)];
-        [self addSubview:lbl];
-
-        lbl = [[UILabel alloc]initWithFrame:CGRectMake(kBottomHorizontalMargin, bottom + kVerticalGap, self.frame.size.width - kBottomHorizontalMargin * 2, kBottomLabelFontSize * 1.2)];
+        lbl = [[UILabel alloc] initWithFrame:CGRectMake(kBottomHorizontalMargin, bottom + kVerticalGap, self.frame.size.width - kBottomHorizontalMargin * 2, kBottomLabelFontSize * 1.2)];
         lbl.font = [UIFont systemFontOfSize:kBottomLabelFontSize];
         lbl.textColor = [UIColor whiteColor];
         lbl.textAlignment = NSTextAlignmentRight;
@@ -170,12 +206,12 @@
 
         bottom = CGRectGetMaxY(lbl.frame);
 
-        if(price > 0){
-            lbl = [[UILabel alloc]initWithFrame:CGRectMake(kBottomHorizontalMargin, bottom + kVerticalGap - 4, self.frame.size.width - kBottomHorizontalMargin * 2, kBottomLabelFontSize * 1.2)];
+        if (price > 0) {
+            lbl = [[UILabel alloc] initWithFrame:CGRectMake(kBottomHorizontalMargin, bottom + kVerticalGap - 4, self.frame.size.width - kBottomHorizontalMargin * 2, kBottomLabelFontSize * 1.2)];
             lbl.font = [UIFont systemFontOfSize:kBottomLabelFontSize];
             lbl.textColor = [UIColor whiteColor];
             lbl.textAlignment = NSTextAlignmentRight;
-            lbl.text = [NSString stringWithFormat:@"%@ %.2f", symbol ,(price * cold)/pow(10, 8)];
+            lbl.text = [NSString stringWithFormat:@"%@ %.2f", symbol, (price * cold) / pow(10, 8)];
             [self addSubview:lbl];
 
             bottom = CGRectGetMaxY(lbl.frame);
@@ -186,34 +222,34 @@
     frame.size.height = bottom;
     self.frame = frame;
 
-    UIButton *dismissBtn = [[UIButton alloc]initWithFrame:CGRectMake(0, CGRectGetMaxY(self.chart.frame), self.frame.size.width, self.frame.size.height - CGRectGetMaxY(self.chart.frame))];
+    UIButton *dismissBtn = [[UIButton alloc] initWithFrame:CGRectMake(0, CGRectGetMaxY(self.chart.frame), self.frame.size.width, self.frame.size.height - CGRectGetMaxY(self.chart.frame))];
     [dismissBtn setBackgroundImage:nil forState:UIControlStateNormal];
     dismissBtn.adjustsImageWhenHighlighted = NO;
     [self addSubview:dismissBtn];
     [dismissBtn addTarget:self action:@selector(dismissPressed:) forControlEvents:UIControlEventTouchUpInside];
 }
 
--(void)dialogDidShow{
+- (void)dialogDidShow {
     [super dialogDidShow];
-    [self.chart setAmounts:@[@(hdm), @(hot), @(cold)]];
+    [self.chart setAmounts:@[@(hd), @(hdm), @(hot), @(cold)]];
     [self.chart setNeedsDisplay];
 
 }
 
--(void)dialogDidDismiss{
+- (void)dialogDidDismiss {
     [super dialogDidDismiss];
-    if(self.listener && [self.listener respondsToSelector:@selector(dialogDismissed)]){
+    if (self.listener && [self.listener respondsToSelector:@selector(dialogDismissed)]) {
         [self.listener dialogDismissed];
     }
 }
 
--(void)dismissPressed:(id)sender{
+- (void)dismissPressed:(id)sender {
     [self dismiss];
 }
 
--(NSAttributedString*)stringAddDotColor:(UIColor*)color string:(NSString*)str{
-    UIImage* image = [[UIImage imageNamed:@"dialog_total_balance_dot"] renderToColor:color];
-    NSMutableAttributedString *attr = [[NSMutableAttributedString alloc]initWithString:str];
+- (NSAttributedString *)stringAddDotColor:(UIColor *)color string:(NSString *)str {
+    UIImage *image = [[UIImage imageNamed:@"dialog_total_balance_dot"] renderToColor:color];
+    NSMutableAttributedString *attr = [[NSMutableAttributedString alloc] initWithString:str];
     [attr addAttribute:NSFontAttributeName value:[UIFont systemFontOfSize:kBottomLabelFontSize] range:NSMakeRange(0, attr.length)];
     NSTextAttachment *attachment = [[NSTextAttachment alloc] init];
     attachment.image = image;
@@ -224,12 +260,12 @@
     attachment.bounds = bounds;
 
     NSAttributedString *attachmentString = [NSAttributedString attributedStringWithAttachment:attachment];
-    [attr insertAttributedString:[[NSAttributedString alloc]initWithString:@" "] atIndex:0];
+    [attr insertAttributedString:[[NSAttributedString alloc] initWithString:@" "] atIndex:0];
     [attr insertAttributedString:attachmentString atIndex:0];
     return attr;
 }
 
--(BOOL)arrowAlwaysOnTop{
+- (BOOL)arrowAlwaysOnTop {
     return YES;
 }
 
