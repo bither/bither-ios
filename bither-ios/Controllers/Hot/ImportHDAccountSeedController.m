f5e4669d6129d60d19fd7997c0bbdfaa52ca1028
songchenwen <sanvinsung@gmail.com>
2015-07-16 14:52:40 +0800

    Import Cold HD Account

--- bither-ios/Controllers/Hot/ImportHDAccountSeedController.m
+++ bither-ios/Controllers/Hot/ImportHDAccountSeedController.m
@@ -167,16 +167,26 @@
                 });
                 return;
             }
-            BTHDAccount *account = [[BTHDAccount alloc] initWithMnemonicSeed:mnemonicCodeSeed password:password fromXRandom:NO syncedComplete:NO andGenerationCallback:nil];
-            if (!account) {
-                dispatch_async(dispatch_get_main_queue(), ^{
-                    [self showMsg:NSLocalizedString(@"import_hdm_cold_seed_format_error", nil)];
-                });
-                return;
+            if ([BTSettings instance].getAppMode == HOT) {
+                BTHDAccount *account = [[BTHDAccount alloc] initWithMnemonicSeed:mnemonicCodeSeed password:password fromXRandom:NO syncedComplete:NO andGenerationCallback:nil];
+                if (!account) {
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        [self showMsg:NSLocalizedString(@"import_hdm_cold_seed_format_error", nil)];
+                    });
+                    return;
+                }
+                [[PeerUtil instance] stopPeer];
+                [BTAddressManager instance].hdAccountHot = account;
+                [[PeerUtil instance] startPeer];
+            } else {
+                BTHDAccountCold *account = [[BTHDAccountCold alloc] initWithMnemonicSeed:mnemonicCodeSeed andPassword:password];
+                if (!account) {
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        [self showMsg:NSLocalizedString(@"import_hdm_cold_seed_format_error", nil)];
+                    });
+                    return;
+                }
             }
-            [[PeerUtil instance] stopPeer];
-            [BTAddressManager instance].hdAccountHot = account;
-            [[PeerUtil instance] startPeer];
             dispatch_async(dispatch_get_main_queue(), ^{
                 [dp dismissWithCompletion:^{
                     [s.navigationController popViewControllerAnimated:YES];
