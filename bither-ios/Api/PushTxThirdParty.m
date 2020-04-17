48e6a58d53c5f7606a4d4af5b7c18d2c7e1d3913
hanzhenzhen <hanzhenzhen666@gmail.com>
2020-04-17 17:16:41 +0800

    push tx add chain.api.btc.com

--- bither-ios/Api/PushTxThirdParty.m
+++ bither-ios/Api/PushTxThirdParty.m
@@ -16,21 +16,22 @@ static PushTxThirdParty* instance;
     AFHTTPSessionManager* manager;
 }
 
-+(instancetype)instance{
++(instancetype)instance {
     if(!instance){
         instance = [[PushTxThirdParty alloc]init];
     }
     return instance;
 }
 
--(instancetype)init{
+-(instancetype)init {
     if (!(self = [super init])) return nil;
     manager = [AFHTTPSessionManager manager];
     return self;
 }
 
--(void)pushTx:(BTTx*) tx {
+-(void)pushTx:(BTTx *)tx {
     NSString* raw = [NSString hexWithData:tx.toData];
+    [self pushToChianBtcCom:raw];
     [self pushToBlockChainInfo:raw];
     [self pushToBtcCom:raw];
     [self pushToChainQuery:raw];
@@ -47,23 +48,27 @@ static PushTxThirdParty* instance;
     }];
 }
 
--(void)pushToBlockChainInfo:(NSString*) raw {
+-(void)pushToBlockChainInfo:(NSString *)raw {
     [self pushToUrl:@"https://blockchain.info/pushtx" key:@"tx" rawTx:raw tag:@"blockchain.info"];
 }
 
--(void)pushToBtcCom:(NSString*) raw {
+-(void)pushToBtcCom:(NSString *)raw {
     [self pushToUrl:@"https://btc.com/api/v1/tx/publish" key:@"hex" rawTx:raw tag:@"BTC.com"];
 }
 
--(void)pushToChainQuery:(NSString*) raw {
+-(void)pushToChianBtcCom:(NSString *)raw {
+    [self pushToUrl:@"https://chain.api.btc.com/v3/tools/tx-publish" key:@"rawhex" rawTx:raw tag:@"ChianBTC.com"];
+}
+
+-(void)pushToChainQuery:(NSString *)raw {
     [self pushToUrl:@"https://chainquery.com/bitcoin-api/sendrawtransaction" key:@"transaction" rawTx:raw tag:@"ChainQuery.com"];
 }
 
--(void)pushToBlockr:(NSString*)raw {
+-(void)pushToBlockr:(NSString *)raw {
     [self pushToUrl:@"https://blockr.io/api/v1/tx/push" key:@"hex" rawTx:raw tag:@"blockr.io"];
 }
 
--(void)pushToBlockExplorer:(NSString*)raw {
+-(void)pushToBlockExplorer:(NSString *)raw {
     [self pushToUrl:@"https://blockexplorer.com/api/tx/send" key:@"rawtx" rawTx:raw tag:@"BlockExplorer"];
 }
 
