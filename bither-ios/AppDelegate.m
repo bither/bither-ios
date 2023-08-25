dbbe8e1d9f8193365858877c2987e57379625571
hanzhenzhen <hanzhenzhen666@gmail.com>
2023-08-25 09:37:09 +0800

    modify to only use the system keyboard

--- bither-ios/AppDelegate.m
+++ bither-ios/AppDelegate.m
@@ -209,6 +209,10 @@ static StatusBarNotificationWindow *notificationWindow;
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 }
 
+- (BOOL)application:(UIApplication *)application shouldAllowExtensionPointIdentifier:(UIApplicationExtensionPointIdentifier)extensionPointIdentifier {
+    return NO;
+}
+
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
